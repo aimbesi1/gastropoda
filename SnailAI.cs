c94e756aa1989cec8f46e6e86dda91e28f2f896f
Jimmy Haboon <101476866+Jhaboon21@users.noreply.github.com>
2022-09-17 01:59:10 -0400

    Update SnailAI.cs
    Tracks player on spawn and increase speed over a timer

--- SnailAI.cs
+++ SnailAI.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class SnailAI : MonoBehaviour
 {
     public GameObject player;
-    public float speed;
+    public Rigidbody2D rigidbody2D;
+    public float speed = 0;
 
     private float distance;
 
+    //Snail will get faster with increments
+    public float increaseSpeedTimer = 5f; // After a set timer of seconds
+    public float speedToIncreaseBy = 1f;  // Increase the snail speed by this
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player"); //When snail spawns, it will find the player
+        InvokeRepeating("DoTimer", 0.001f, increaseSpeedTimer);
+    }
+
+    private void DoTimer()
+    {
+        gameObject.SendMessage("IncreaseSpeed", speedToIncreaseBy);
+    }
+
+    private void IncreaseSpeed(float s)
+    {
+        speed += s;
+    }
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed *Time.deltaTime);
-
     }
 }

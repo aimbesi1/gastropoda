f0532f3eb475f69409f4285652ac4c60d4bec864
Kairu1206 <kylenguyen1206@gmail.com>
2022-10-07 18:21:38 -0400

    SaltGun mechanics work

--- Assets/Scripts/SaltShakerGun.cs
+++ Assets/Scripts/SaltShakerGun.cs
@@ -7,16 +7,7 @@ public class SaltShakerGun : MonoBehaviour
     private int num_bullet = 10;
     public Transform firepoint;
     public GameObject Bullet;
-    List<Transform> bullets;
-    // Start is called before the first frame update
-    void Awake()
-    {
-        bullets = new List<Transform>(num_bullet);
-        for(int i = 0; i < num_bullet; i++)
-        {
-            bullets.Add(Quaternion.Euler(Vector2.zero));
-        }
-    }
+    private int bulletVel = 100;
 
     // Update is called once per frame
     void Update()
@@ -29,10 +20,23 @@ public class SaltShakerGun : MonoBehaviour
 
     void shoot()
     {
-        int i = 0;
-        foreach(Quaternion quat in bullets)
+        for(int i = 0; i <= 2; i++)
         {
-            
+            GameObject b = Instantiate(Bullet, firepoint.position, firepoint.rotation);
+
+            switch(i)
+            {
+                case 0:
+                    b.GetComponent<Rigidbody2D>().AddForce(firepoint.right * bulletVel + new Vector3(0, -30f, 0));
+                    break;
+                case 1:
+                    b.GetComponent<Rigidbody2D>().AddForce(firepoint.right * bulletVel + new Vector3(0, 0, 0));
+                    break;
+                case 2:
+                    b.GetComponent<Rigidbody2D>().AddForce(firepoint.right * bulletVel + new Vector3(0, 30f, 0));
+                    break;
+
+            }
         }
     }
 }

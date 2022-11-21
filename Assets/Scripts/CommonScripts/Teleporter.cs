236951afc7a4e708eab79971b130c26c596c5fc2
Kairu1206 <kylenguyen1206@gmail.com>
2022-11-21 13:31:59 -0500

    bug fixed

--- Assets/Scripts/CommonScripts/Teleporter.cs
+++ Assets/Scripts/CommonScripts/Teleporter.cs
@@ -7,37 +7,41 @@ public class Teleporter : MonoBehaviour
 {
     public string levelName;
 
+    void Awake()
+    {
+        PlayerPrefs.SetString("CurrentScene", levelName);
+    }
+
     public void LoadLevel()
     {
         SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Single);
-        PlayerPrefs.SetString("CurrentScene", levelName);
     }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         if(hitInfo.CompareTag("Player"))
         {
-            if (levelName == "Level 2.1") // Player will not have weapons when change era
-            {
-                PlayerPrefs.SetInt("PlayerMaxHealth", 100); // Set player max health
-                PlayerPrefs.SetInt("PlayerCurrentHealth", PlayerPrefs.GetInt("PlayerMaxHealth")); // set player initial health to max health
-                // Intialize Player with no weapon
-                PlayerPrefs.SetInt("HasGun", 0);
-                PlayerPrefs.SetInt("HasSword", 0);
-                PlayerPrefs.SetInt("HasSaltGun", 0);
-
-                PlayerPrefs.SetInt("Clip", 3); // Store the number of clip in the normal gun
-                PlayerPrefs.SetInt("Ammo", 12); //Store the number of bullet in the normal gun
-                PlayerPrefs.SetInt("Throwtime", 2); // Store the throw time for the sword
-                PlayerPrefs.SetInt("Shoottime", 10); // Store the shoot time of the salt gun
-            }
-            if (levelName == "Level 2.1" || levelName == "Level 2.3" || levelName == "Level 2.6")
+            if (levelName == "Level 2.1" || levelName == "Level 2.4" || levelName == "End Game Scene")
             {
                 PlayerPrefs.SetInt("IsBoss", 1); // Set the snail as a boss
             }
             
             if(PlayerPrefs.GetInt("IsBoss") == 0)
             {
+                if (levelName == "Level 2.1") // Player will not have weapons when change era
+                {
+                    PlayerPrefs.SetInt("PlayerMaxHealth", 100); // Set player max health
+                    PlayerPrefs.SetInt("PlayerCurrentHealth", PlayerPrefs.GetInt("PlayerMaxHealth")); // set player initial health to max health
+                    // Intialize Player with no weapon
+                    PlayerPrefs.SetInt("HasGun", 0);
+                    PlayerPrefs.SetInt("HasSword", 0);
+                    PlayerPrefs.SetInt("HasSaltGun", 0);
+
+                    PlayerPrefs.SetInt("Clip", 3); // Store the number of clip in the normal gun
+                    PlayerPrefs.SetInt("Ammo", 12); //Store the number of bullet in the normal gun
+                    PlayerPrefs.SetInt("Throwtime", 2); // Store the throw time for the sword
+                    PlayerPrefs.SetInt("Shoottime", 10); // Store the shoot time of the salt gun
+                }
                 LoadLevel();
             }
             else

9824183789a1e44751e535f53f9b231bea405319
Spade <47337405+SagarPateI@users.noreply.github.com>
2022-12-02 13:20:47 -0500

    Quang's changes

--- Assets/Scripts/CommonScripts/EndGame.cs
+++ Assets/Scripts/CommonScripts/EndGame.cs
@@ -9,6 +9,11 @@ public class EndGame : MonoBehaviour
 
     public void Respawn()
     {
+        PlayerPrefs.SetInt("PlayerMaxHealth", 100); // Set player max health
+        PlayerPrefs.SetInt("PlayerCurrentHealth", PlayerPrefs.GetInt("PlayerMaxHealth")); // set player initial health to max health
+
+        PlayerPrefs.SetInt("PlayerCurrentShield", 0); // Set player initial shield to 0
+        
         SceneManager.LoadSceneAsync(PlayerPrefs.GetString("CurrentScene"), LoadSceneMode.Single);
     }
 

a464ec71bd4da3b701f230e509790fd94c242c47
Kairu1206 <kylenguyen1206@gmail.com>
2022-12-02 12:43:14 -0500

    v1.0.2

--- Assets/Scripts/ObjectScripts/Ladder.cs
+++ Assets/Scripts/ObjectScripts/Ladder.cs
@@ -11,6 +11,10 @@ public class Ladder : MonoBehaviour
 
     [SerializeField] private Rigidbody2D rb;
 
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
